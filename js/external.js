e3d9cf9e2cee77f2686dafca46f74c88d77cacac
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-05-30 14:07:27 -0500

    add javascript

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log("Hello from external JavaScript")
+alert('Welcome to my Website!')
+let favoriteColor = prompt("Whats your favorite color?");
+alert("awesome my favorite color is " + favoriteColor + " too")
+
+
+
