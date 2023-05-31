2b8b2b053d598acc5b5898bdcb845c161c156d4e
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-05-31 13:31:23 -0500

    first commit

---
+++ js/functions-lecture.js
@@ -0,0 +1,55 @@
+// function sayHello(){console.log("HELLO")
+//    };
+// function sayBye(){
+//     console.log("GOOD BYE")
+// }
+// // sayHello();
+// //
+// // sayBye();
+// function sayHelloUser(usersUserName){
+//     console.log("HELLO " + usersUserName)
+// }
+// sayHelloUser("Kurt")
+//
+// function sayByeUser(usersUserName){
+//     console.log("GOOD BYE " + usersUserName)
+// }
+// sayByeUser("Kurt")
+//
+// // Global Scope example
+// const username = "Kurt"
+//
+// function sayBye(){
+//     console.log("you will always see me " + username)
+// }
+// sayBye()
+// Local Scope Example
+// const username = "Kurt"
+//
+// function sayBye(){
+//     console.log("you will always see me " + username)
+// }
+// sayBye()
+//
+// function addFourDividedByTwo(num){
+//     const sum = num + 4;
+//     return dividedByTwo}
+//     function dividedByTwo(num){
+//         const sum = num / 2;
+// }
+// addFourDividedByTwo(6)
+//     console.log(addFourDividedByTwo(6))
+//My Trail function
+    // Immediately Invoked Function Expression
+// let result =(function(){
+//    let x = 3
+//    let y = 4
+//     return x + y;
+// }();
+
+
+
+
+
+
+
