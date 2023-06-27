40785689de77ba7a57d8580db5a195c66f5bc59d
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-06-27 15:34:41 -0500

    warm up & fizzbuzz loop practice

--- js/1on1JS.js
+++ js/1on1JS.js
@@ -32,10 +32,50 @@ function returnFirstLetters(salesData) {
     }
     return word
 }
-returnFirstLetters(salesData)
-console.log(returnFirstLetters(salesData));
+
+// returnFirstLetters(salesData)
+// console.log(returnFirstLetters(salesData));
 
 
+// solving with traditional for loop
+// let result = "";
+// for(let i = 0; i < salesData.length; i += 1){
+//     let month = salesData[i].month;
+//     let firstLetter = month.charAt(0);
+//     result += firstLetter;
+// }
+//console.log(result);
 
+// solving with forEach method
+let result = "";
+
+// using traditional anonymous function
+// salesData.forEach(function(sale){
+//     let month = sale.month;
+//     let firstLetter = month.charAt(0);
+//     result += firstLetter;
+// });
+
+// using anonymous arrow function
+// salesData.forEach(sale => result += sale.month.charAt(0));
+
+// // using for of
+// for(let sale of salesData) {
+//     result += sale.month.charAt(0);
+// }
+// console.log(result);
+
+// fizzBuzz
+for (let x = 0; x <= 100; x++) {
+    if (x % 15 === 0) {
+        console.log('Fizzbuzz')
+    } else if (x % 5 === 0) {
+        console.log('Buzz')
+    } else if (x % 3 === 0) {
+        console.log('Fizz')
+    } else {
+        console.log(x)
+    }
+}
 
 
