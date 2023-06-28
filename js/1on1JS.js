9011f98a3eddd1a5050c4be3752d8b8651bd458f
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-06-28 15:55:40 -0500

    added icons

--- js/1on1JS.js
+++ js/1on1JS.js
@@ -66,16 +66,57 @@ let result = "";
 // console.log(result);
 
 // fizzBuzz
-for (let x = 0; x <= 100; x++) {
-    if (x % 15 === 0) {
-        console.log('Fizzbuzz')
-    } else if (x % 5 === 0) {
-        console.log('Buzz')
-    } else if (x % 3 === 0) {
-        console.log('Fizz')
-    } else {
-        console.log(x)
+// for (let x = 0; x <= 100; x++) {
+//     if (x % 15 === 0) {
+//         console.log('Fizzbuzz')
+//     } else if (x % 5 === 0) {
+//         console.log('Buzz')
+//     } else if (x % 3 === 0) {
+//         console.log('Fizz')
+//     } else {
+//         console.log(x)
+//     }
+// }
+
+//Exercise 1: Print Numbers
+// Write a loop that prints numbers from 1 to 10.
+
+for (let i = 1; i <= 10; i += 1) {
+    console.log(i)
+}
+// Exercise 2: Even Numbers
+// Write a loop that prints even numbers from 2 to 20.
+
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+// Exercise 3: Sum of Numbers
+// Write a loop that calculates the sum of numbers from 1 to 100 and prints the result.
+let sum = 0
+for(let i = 1 ; i <= 100; i += 1){
+    sum += i
+}
+console.log(sum)
+
+
+// Exercise 4: Reverse Loop
+// Write a loop that prints numbers from 10 to 1 in reverse order.
+ for(let i = 10 ; i >= 0;i--){
+     console.log(i)
+
+ }
+
+
+// Exercise 5: Multiplication Table
+// Write a loop that generates a multiplication table for a given number. For example, if the number is 5, the table should print:
+function multiplicationTable(number) {
+    for (let i = 1; i <= 10; i++) {
+        let result = number * i;
+        console.log(number + " x " + i + " = " + result);
     }
 }
+ multiplicationTable(5)
+
 
 
