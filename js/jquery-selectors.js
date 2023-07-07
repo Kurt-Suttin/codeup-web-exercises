56147991c53d7f164c0e2490c68819f6d403c3a3
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-07 16:36:23 -0500

    completed selector exercise

---
+++ js/jquery-selectors.js
@@ -0,0 +1,23 @@
+// $(()=>{
+//     $("#id-container").css('background-color','red');
+// })
+// $(".title").css('color','green');
+
+// with Js
+// let titleElements = document.querySelectorAll('.title');
+// titleElements.forEach(titleElement =>{
+//     titleElement.style.color = "blue"
+// });
+
+// use jQuery to select all 'li' elements
+
+$('li').css('font-size','20px');
+
+
+// use j query to select multiple selectors
+// $('h1,p,.title,#id-container').css('font-weight','bold')
+
+// use jQuery to select all
+// $('*').css('color','pink')
+
+// $('.codeup').css('border','1px solid red')
