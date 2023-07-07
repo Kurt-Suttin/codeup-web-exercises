febf9d666cc75383fcdc0cdf90a0c61bfb553117
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-07 14:17:30 -0500

    comment in jquery exercise and created selector exercise files

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -4,7 +4,14 @@
 //     alert("window has loaded")
 // };
 
+
 /// JQuery function alert when page loads
-jQuery(function(){
+// jQuery(function(){
+//     alert("window has finished loading , click ok to continue.");
+// });
+
+
+/// $ = jQuery
+$(function(){
     alert("window has finished loading , click ok to continue.");
 });
