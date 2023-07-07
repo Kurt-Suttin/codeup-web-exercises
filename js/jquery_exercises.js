3833a37b58ab5afa8525933b9445d98a314438eb
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-07 14:02:26 -0500

    j query exercise completed

---
+++ js/jquery_exercises.js
@@ -0,0 +1,10 @@
+
+/// Js function alert when page loads
+// window.onload = function (){
+//     alert("window has loaded")
+// };
+
+/// JQuery function alert when page loads
+jQuery(function(){
+    alert("window has finished loading , click ok to continue.");
+});
