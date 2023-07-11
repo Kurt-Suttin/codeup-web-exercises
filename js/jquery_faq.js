28718ac8cd82f60c137e171ac0431d35888078ab
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-11 14:00:04 -0500

    added more function to chnage background

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,8 +1,14 @@
 "use strict"
 
-
+// DD function
 $('a').click(function (event) {
     $('dd').toggleClass('invisible');
+    $('body').css("background-color", "#caebe2")
 });
 
+// DT function
+$("dt").on("click", function (e) {
+    $(e.target).css("background-color", "yellow")
+
+})
 
