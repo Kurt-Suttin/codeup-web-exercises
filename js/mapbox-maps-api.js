4477c724fec015554800872fdbd6d2e65b298471
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-14 11:28:31 -0500

    mapbox-maps-api exercise 1-7 completed

--- js/mapbox-maps-api.js
+++ js/mapbox-maps-api.js
@@ -14,35 +14,35 @@ $(() => {
         const mapOptions = {
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v12',
-            zoom: 10,
-            center: [-98.4916, 29.4252],
+            zoom: 15,
+            center: [-97.75206800629019, 30.267686234202444],
         }
 
         return new mapboxgl.Map(mapOptions);
     }
 
-    // Function that creates a marker at Codeup
+    // Function that creates a marker at Restaurant
     function createMarker() {
         return new mapboxgl.Marker()
-            .setLngLat([-98.4916, 29.4260])
-            .addTo(map);
+
     }
 
     // Function that creates a popup
     function createPopup() {
         return new mapboxgl.Popup()
-            .setLngLat([-98.489615, 29.426827])
+            .setLngLat([-97.75206800629019, 30.267686234202444])
+
             .setHTML(`
                 <div>
-                    <h1>Codeup</h1>
+                    <h1>Restaurant</h1>
                     <p>We can put anything we want</p>
                 </div>
             `);
     }
 
-    // Function that brings me to paris
-    function goToParis() {
-        geocode('Statue of Liberty', MAPBOX_TOKEN).then((data) => {
+    // Function that brings me to True Food Kitchen, Austin
+    function goToFavoriteRestaurant() {
+        geocode(`True Food Kitchen, Austin`, MAPBOX_TOKEN).then((data) => {
             console.log(data);
             map.setCenter(data);
         })
@@ -61,10 +61,10 @@ $(() => {
 
     // Function that uses geocode to take the string 'The Alamo, San Antonio'
     // and get coordinates to set a marker and popup at that location
-    function markAlamo() {
-        geocode('The Alamo, San Antonio', MAPBOX_TOKEN).then((data) => {
+    function markRestaurant() {
+        geocode('True Food Kitchen, Austin', MAPBOX_TOKEN).then((data) => {
             const alamoPopup = new mapboxgl.Popup()
-                .setHTML(`<p>Remember the Alamo!</p>`);
+                .setHTML(`<p>True Food Kitchen, Austin</p>`);
             const alamoMarker = new mapboxgl.Marker()
                 .setLngLat(data)
                 .addTo(map)
@@ -74,19 +74,34 @@ $(() => {
     }
 
 
-
-
     // Events
-    document.querySelector('#geocode-button').addEventListener('click', goToParis);
+    document.querySelector('#geocode-button').addEventListener('click', goToFavoriteRestaurant);
     document.querySelector('#reverse-geocode-button').addEventListener('click', findAndPrintAddress);
-    document.querySelector('#mark-alamo').addEventListener('click', markAlamo);
+    document.querySelector('#mark-restaurant').addEventListener('click', markRestaurant);
 
     // Runs when the program loads
     // map.setZoom(1);
     marker.setPopup(popup);
 
+    //Refactor your code to display at
+    // least three of your favorite restaurants with information about each.
+    // Create an array of objects with information about each restaurant to accomplish this.
+    // Use a .forEach() loop rather than a for loop.
 
+    const restaurants = [{
+        title: 'Whataburger',
+        address: '412 E Commerce St, San Antonio, TX 78205',
+        rating: '1.5 stars',
+    }, {
+        title: 'Chart House',
+        address: '739 E César E. Chávez Blvd, San Antonio, TX 78205',
+        rating: '5 stars',
+    }, {
+        title: `Bohanan's Prime Steaks and Seafood`,
+        address: ' 219 E Houston St #275, San Antonio, TX 78205',
+        rating: '5 stars',
+    }
+    ];
 
-
-
+restaurants.forEach(restaurant )
 });
