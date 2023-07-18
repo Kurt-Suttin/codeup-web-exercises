31b7ce6bd0cfc068e785768f41ef2cc0fce09807
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-18 13:27:03 -0500

    added konami code

--- js/ent-weather-map.js
+++ js/ent-weather-map.js
@@ -226,5 +226,60 @@ marker.on('dragend', (e) => {
     })
 
 });
+////////KONAMI CODE//////
 
+let $easteregg = $("#easter-egg");
+let $cofetti = $(".confetti")
+// number vaule when keys are pressed
+const allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'a',
+    66: 'b',
+    13: 'enter'
+};
+// the 'official' Konami Code sequence
+let konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a','enter'];
+
+// a variable to remember the 'position' the user has reached so far.
+let konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener('keydown', function (e) {
+    // get the value of the key code from the key map
+    let key = allowedKeys[e.keyCode];
+    // get the value of the required key from the konami code
+    let requiredKey = konamiCode[konamiCodePosition];
+
+    // compare the key with the required key
+    if (key == requiredKey) {
+
+        // move to the next key in the konami code sequence
+        konamiCodePosition++;
+
+        // if the last key is reached, activate cheats
+        if (konamiCodePosition == konamiCode.length) {
+            activateCheats();
+            konamiCodePosition = 0;
+        }
+    } else {
+        konamiCodePosition = 0;
+    }
+});
+
+function activateCheats() {
+    document.body.style.backgroundImage = "url('img')";
+    $easteregg.css('background-color', 'pink');
+    $cofetti.css('opacity', '100%')
+    let sound = new Audio('audio/grunt.mp3');
+    sound.play();
+    console.log("cheats activated")
+}
+
+
+$(document).keyup(function (event) {
+    console.log(event.keyCode);
+});
 
